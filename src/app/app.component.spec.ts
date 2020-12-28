a828c724c324bc9996fe90f71d68b1693ae0d818
Russell Morrisey <Russell.Morrisey@cscglobal.com>
2020-12-28 13:46:44 -0500

    Implement broken unit test.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,10 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {DataService} from './data.service';
+import {GridComponent, GridModule} from '@progress/kendo-angular-grid';
+import {ReactiveFormsModule} from '@angular/forms';
+import {By} from '@angular/platform-browser';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -7,6 +12,14 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        DataService
+      ],
+      imports: [
+        GridModule,
+        ReactiveFormsModule,
+        NoopAnimationsModule
+      ]
     }).compileComponents();
   });
 
@@ -16,16 +29,48 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ex-inline-edit'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ex-inline-edit');
-  });
-
-  it('should render title', () => {
+  it('should render the grid', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ex-inline-edit app is running!');
+    const kendoGridEl = fixture.debugElement.query(By.directive(GridComponent));
+    expect(kendoGridEl).toBeTruthy();
+
+    const gridRows = kendoGridEl.queryAll(By.css('tbody[kendogridtablebody] tr[role=row]'));
+    expect(gridRows).toBeTruthy();
+    expect(gridRows.length).toEqual(1);
+
+    const rowValueLabel = gridRows[0].nativeElement.innerText;
+    expect(rowValueLabel).toEqual('bar');
+  });
+
+  it('should support inline editing', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const kendoGridEl = fixture.debugElement.query(By.directive(GridComponent));
+    expect(kendoGridEl).toBeTruthy();
+
+    const gridRows = kendoGridEl.queryAll(By.css('tbody[kendogridtablebody] tr[role=row]'));
+    expect(gridRows).toBeTruthy();
+    expect(gridRows.length).toEqual(1);
+
+    // 1 - Trigger inline editing
+    const gridCell = gridRows[0].query(By.css('td[kendogridlogicalcell]'));
+    expect(gridCell).toBeTruthy();
+    // FIXME: this doesn't work!!
+    gridCell.triggerEventHandler('click', {});
+    fixture.detectChanges();
+
+    // 2 - Change the value
+    const rowEditFormGroup = fixture.componentInstance.formGroup;
+    expect(rowEditFormGroup).toBeTruthy();
+    const editControl = rowEditFormGroup?.controls[fixture.componentInstance.FIELD_NAME];
+    expect(editControl).toBeTruthy();
+    editControl?.setValue('edited');
+    editControl?.markAsDirty();
+    fixture.detectChanges();
+
+    // 3 - verify the results
+    const dataService = TestBed.inject(DataService);
+    expect(dataService.data$.value).toEqual([{foo: 'edited'}]);
   });
 });
--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -2,7 +2,8 @@ import { TestBed } from '@angular/core/testing';
 
 import { DataService } from './data.service';
 
-describe('DataService', () => {
+// not relevant to the issue
+xdescribe('DataService', () => {
   let service: DataService;
 
   beforeEach(() => {

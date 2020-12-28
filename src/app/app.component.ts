a1be36e5156d54cac013b55f2563d49cf13b7c80
Russell Morrisey <Russell.Morrisey@cscglobal.com>
2020-12-28 13:33:44 -0500

    Fixed form submit issue and persist changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,24 @@
-import {Component, ContentChild, ViewChild} from '@angular/core';
+import {Component, ContentChild, OnInit} from '@angular/core';
 import {CellClickEvent, CellCloseEvent, GridComponent} from '@progress/kendo-angular-grid';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {DataService} from './data.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  public DATA = [
-    {foo: 'bar'}
-  ];
+export class AppComponent implements OnInit {
   public FIELD_NAME = 'fooCustomControl';
   public formGroup?: FormGroup;
   public result: string = '';
   @ContentChild(GridComponent)
   public grid?: GridComponent;
+  private data$?: Observable<{ foo: string }[]>;
 
   constructor(
+    private dataService: DataService,
     private formBuilder: FormBuilder
   ) {
   }
@@ -29,10 +30,10 @@ export class AppComponent {
 
   onCellClose(event: CellCloseEvent) {
     this.result = this.formGroup?.value[this.FIELD_NAME];
+    this.dataService.data$.next([{...event.dataItem, foo: this.result}]);
   }
 
-  onSubmit($event: any) {
-    console.log('submitted');
-    this.grid?.closeCell();
+  ngOnInit(): void {
+    this.data$ = this.dataService.data$;
   }
 }

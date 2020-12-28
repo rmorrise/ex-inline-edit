5488173479cc25da6a91a8af442de9812f75a7e1
Russell Morrisey <Russell.Morrisey@cscglobal.com>
2020-12-28 12:04:47 -0500

    Sample project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ContentChild, ViewChild} from '@angular/core';
+import {CellClickEvent, CellCloseEvent, GridComponent} from '@progress/kendo-angular-grid';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ex-inline-edit';
+  public DATA = [
+    {foo: 'bar'}
+  ];
+  public FIELD_NAME = 'fooCustomControl';
+  public formGroup?: FormGroup;
+  public result: string = '';
+  @ContentChild(GridComponent)
+  public grid?: GridComponent;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+  }
+
+  onCellClick(event: CellClickEvent) {
+    this.formGroup = this.formBuilder.group({[this.FIELD_NAME]: [event.dataItem.foo]});
+    event.sender.editCell(event.rowIndex, event.columnIndex, this.formGroup);
+  }
+
+  onCellClose(event: CellCloseEvent) {
+    this.result = this.formGroup?.value[this.FIELD_NAME];
+  }
+
+  onSubmit($event: any) {
+    console.log('submitted');
+    this.grid?.closeCell();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    GridModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
